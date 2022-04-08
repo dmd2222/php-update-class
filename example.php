83e752e48a207dfae2e893c1b5976728ca03fdca
dmd2222 <dmd20@gmx.de>
2022-04-08 21:57:15 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,11 +1,15 @@
 <?php
 require_once("updater_class.php");
 
+
+//Check update: true/false
 var_dump("New update?:",updater::check_new_update("example.php","https://raw.githubusercontent.com/dmd2222/php-update-class/main/example.php",true));
 
 echo "<br>";
 echo "<br>";
 
+
+//Make update of file
 if($_GET["update"]=="yes"){
         updater::do_file_update("example.php","https://raw.githubusercontent.com/dmd2222/php-update-class/main/example.php",true,true,true,50);
 }
@@ -14,7 +18,19 @@ if($_GET["update"]=="yes"){
 echo "<br>";
 echo "<br>";
 
-
+//Check update and inform admin over update: true/false
 var_dump("New update?:",updater::check_new_update_if_new_inform_admin("example.php","https://raw.githubusercontent.com/dmd2222/php-update-class/main/example.php",true));
+
+
+
+
+echo "<br>";
+echo "<br>";
+
+
+//Make updater object
+$a = new updater("example.php","https://raw.githubusercontent.com/dmd2222/php-update-class/main/example.php",true,true,true,50);
+
+
 
 ?>
