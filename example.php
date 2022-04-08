6405db78b5c023b24fcdf699335c1f6d9540301f
dmd2222 <dmd20@gmx.de>
2022-04-08 21:17:59 +0200

    Update example.php

--- example.php
+++ example.php
@@ -4,7 +4,7 @@ require_once("updater_class.php");
 var_dump("New update?:",updater::check_new_update("example.php","https://raw.githubusercontent.com/dmd2222/php-update-class/main/example.php",true));
 
 if($_GET["update"]=="yes"){
-        updater::do_file_update("example.php","https://raw.githubusercontent.com/dmd2222/php-update-class/main/example.php",true);
+        updater::do_file_update("example.php","https://raw.githubusercontent.com/dmd2222/php-update-class/main/example.php",true,true,true,50);
 }
 
 var_dump("New update?:",updater::check_new_update_if_new_inform_admin("example.php","https://raw.githubusercontent.com/dmd2222/php-update-class/main/example.php",true));
