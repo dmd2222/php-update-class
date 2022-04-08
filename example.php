e56b165001e98707c346f169c9368091ef123083
dmd2222 <dmd20@gmx.de>
2022-04-08 22:09:56 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,7 +1,6 @@
 <?php
 require_once("updater_class.php");
 
-#Version 1.0.0.1
 
 //Check update: true/false
 var_dump("New update?:",updater::check_new_update("example.php","https://raw.githubusercontent.com/dmd2222/php-update-class/main/example.php",true));
@@ -20,7 +19,7 @@ echo "<br>";
 echo "<br>";
 
 //Check update and inform admin over update: true/false
-var_dump("New update?:",updater::check_new_update_if_new_inform_admin("example.php","https://raw.githubusercontent.com/dmd2222/php-update-class/main/example.php",true));
+var_dump("New update?:",updater::check_new_update_if_new_inform_admin("example.php","https://raw.githubusercontent.com/dmd2222/php-update-class/main/example.php","",true));
 
 
 
