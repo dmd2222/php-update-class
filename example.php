aa4af831d1363b348a4071fb23c195b4dd8c9344
dmd2222 <dmd20@gmx.de>
2022-04-08 22:06:17 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,6 +1,7 @@
 <?php
 require_once("updater_class.php");
 
+#Version 1.0.0.1
 
 //Check update: true/false
 var_dump("New update?:",updater::check_new_update("example.php","https://raw.githubusercontent.com/dmd2222/php-update-class/main/example.php",true));
